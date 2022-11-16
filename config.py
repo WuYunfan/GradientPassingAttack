30c991159772f86a54d1c8a3f413e2e8ea2e0f93
wuyunfan <wuyunfan15@mails.ucas.ac.cn>
2022-11-16 16:16:50 +0800

    update config

--- config.py
+++ config.py
@@ -5,38 +5,38 @@ def get_gowalla_config(device):
 
     model_config = {'name': 'MF', 'embedding_size': 64, 'device': device}
     trainer_config = {'name': 'BPRTrainer', 'optimizer': 'Adam', 'lr': 1.e-3, 'l2_reg': 1.e-3,
-                      'device': device, 'n_epochs': 1000, 'batch_size': 2048, 'dataloader_num_workers': 6,
-                      'test_batch_size': 512, 'topks': [20]}
+                      'device': device, 'n_epochs': 1000, 'batch_size': 2 ** 14, 'dataloader_num_workers': 16,
+                      'test_batch_size': 2048, 'topks': [20]}
     gowalla_config.append((dataset_config, model_config, trainer_config))
 
     model_config = {'name': 'MF', 'embedding_size': 64, 'device': device}
     trainer_config = {'name': 'APRTrainer', 'optimizer': 'Adam', 'lr': 1.e-3, 'l2_reg': 1.e-3,
-                      'eps': 1., 'adv_reg': 0.1,
-                      'device': device, 'n_epochs': 1000, 'batch_size': 2048, 'dataloader_num_workers': 6,
-                      'test_batch_size': 512, 'topks': [20]}
+                      'eps': 1., 'adv_reg': 0.01,
+                      'device': device, 'n_epochs': 1000, 'batch_size': 2 ** 14, 'dataloader_num_workers': 16,
+                      'test_batch_size': 2048, 'topks': [20]}
     gowalla_config.append((dataset_config, model_config, trainer_config))
 
     model_config = {'name': 'LightGCN', 'embedding_size': 64, 'n_layers': 3, 'device': device}
     trainer_config = {'name': 'BPRTrainer', 'optimizer': 'Adam', 'lr': 1.e-3, 'l2_reg': 1.e-4,
-                      'device': device, 'n_epochs': 1000, 'batch_size': 2048, 'dataloader_num_workers': 6,
-                      'test_batch_size': 512, 'topks': [20]}
+                      'device': device, 'n_epochs': 1000, 'batch_size': 2 ** 14, 'dataloader_num_workers': 16,
+                      'test_batch_size': 2048, 'topks': [20]}
     gowalla_config.append((dataset_config, model_config, trainer_config))
 
     model_config = {'name': 'ItemKNN', 'k': 1000, 'device': device}
     trainer_config = {'name': 'BasicTrainer',  'device': device, 'n_epochs': 0,
-                      'test_batch_size': 512, 'topks': [20]}
+                      'test_batch_size': 2048, 'topks': [20]}
     gowalla_config.append((dataset_config, model_config, trainer_config))
 
     model_config = {'name': 'MultiVAE', 'layer_sizes': [64, 32],
                     'device': device, 'dropout': 0.7}
     trainer_config = {'name': 'MLTrainer', 'optimizer': 'Adam', 'lr': 1.e-3, 'l2_reg': 1.e-3, 'kl_reg': 0.2,
-                      'device': device, 'n_epochs': 1000, 'batch_size': 512, 'dataloader_num_workers': 6,
-                      'test_batch_size': 512, 'topks': [20]}
+                      'device': device, 'n_epochs': 1000, 'batch_size': 2048, 'dataloader_num_workers': 16,
+                      'test_batch_size': 2048, 'topks': [20]}
     gowalla_config.append((dataset_config, model_config, trainer_config))
 
     model_config = {'name': 'NeuMF', 'embedding_size': 64, 'device': device, 'layer_sizes': [64, 64, 64]}
     trainer_config = {'name': 'BCETrainer', 'optimizer': 'Adam', 'lr': 1.e-3, 'l2_reg': 1.e-2,
-                      'device': device, 'n_epochs': 1000, 'batch_size': 2048, 'dataloader_num_workers': 6,
+                      'device': device, 'n_epochs': 1000, 'batch_size': 2 ** 12, 'dataloader_num_workers': 16,
                       'test_batch_size': 64, 'topks': [20], 'mf_pretrain_epochs': 100,
                       'mlp_pretrain_epochs': 100, 'max_patience': 100, 'neg_ratio': 4}
     gowalla_config.append((dataset_config, model_config, trainer_config))
