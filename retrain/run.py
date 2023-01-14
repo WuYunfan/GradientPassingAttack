acd78eee03edcaad1c96ced6658cd7baa6f5999b
wuyunfan <wuyunfan15@mails.ucas.ac.cn>
2023-01-14 17:21:26 +0800

    temporary save

--- retrain/run.py
+++ retrain/run.py
@@ -9,16 +9,6 @@ import numpy as np
 import os
 
 
-def cal_overlap_rate_array(rec_items_a, rec_items_b):
-    rates = []
-    for user in range(rec_items_a.shape[0]):
-        set_a = set(rec_items_a[user, :].tolist())
-        set_b = set(rec_items_b[user, :].tolist())
-        rate = 1. * len(set_a & set_b) / rec_items_a.shape[1]
-        rates.append(rate)
-    return np.mean(rates)
-
-
 def cal_recall_set(rec_items_a, rec_items_b):
     rates = []
     for user in range(len(rec_items_a)):
@@ -60,8 +50,10 @@ def initial_parameter(new_trainer, model):
 
 
 def main():
+    seed_list = [0, 42, 2022, 131, 1024]
+
     log_path = __file__[:-3]
-    init_run(log_path, 2021)
+    init_run(log_path, seed_list[0])
 
     device = torch.device('cuda')
     config = get_gowalla_config(device)
@@ -71,7 +63,6 @@ def main():
     writer = SummaryWriter(os.path.join(log_path, 'pre_train'))
     dataset = get_dataset(dataset_config)
     model = get_model(model_config, dataset)
-    initial_embedding = torch.clone(model.embedding.weight.data)
     trainer = get_trainer(trainer_config, dataset, model)
     trainer.train(verbose=True, writer=writer)
     old_rec_items = trainer.get_rec_items('train', None, k=100)
@@ -80,62 +71,77 @@ def main():
     dataset_config['path'] = dataset_config['path'][:-7] + 'time'
     new_dataset = get_dataset(dataset_config)
 
-    writer = SummaryWriter(os.path.join(log_path, 'full_retrain'))
-    new_model = get_model(model_config, new_dataset)
-    with torch.no_grad():
-        new_model.embedding.weight.data[:dataset.n_users, :] = initial_embedding[:dataset.n_users, :]
-        new_model.embedding.weight.data[new_dataset.n_users:new_dataset.n_users + dataset.n_items, :] = \
-            initial_embedding[dataset.n_users:, :]
-    new_trainer = get_trainer(trainer_config, new_dataset, new_model)
-    new_trainer.train(verbose=True, writer=writer)
-    writer.close()
-    print('Full Retrain!')
-    new_trainer.retrain_eval(dataset.n_users, dataset.n_items)
-    full_rec_items = new_trainer.get_rec_items('train', None, k=100)[:dataset.n_users, :]
-    overlap_rate = cal_overlap_rate_array(old_rec_items, full_rec_items)
-    print('Overlap rate between full retrain and w/o retrain: {:.3f}\n'.format(overlap_rate * 100))
-    full_retrain_new_rec_items = get_new_rec_items(full_rec_items, old_rec_items)
+    n_full_retrain = 5
+    full_retrain_new_rec_items = None
+    for i in range(n_full_retrain):
+        writer = SummaryWriter(os.path.join(log_path, 'full_retrain' + str(i)))
+        new_model = get_model(model_config, new_dataset)
+        new_trainer = get_trainer(trainer_config, new_dataset, new_model)
+        new_trainer.train(verbose=False, writer=writer)
+        writer.close()
+        print('Full Retrain ' + str(i) + ' !')
+        new_trainer.retrain_eval(dataset.n_users, dataset.n_items)
+        rec_items = new_trainer.get_rec_items('train', None, k=100)[:dataset.n_users, :]
+        new_rec_items = get_new_rec_items(rec_items, old_rec_items)
+        if full_retrain_new_rec_items is None:
+            full_retrain_new_rec_items = new_rec_items
+        else:
+            for user in range(len(full_retrain_new_rec_items)):
+                full_retrain_new_rec_items[user] &= new_rec_items[user]
 
-    writer = SummaryWriter(os.path.join(log_path, 'part_retrain'))
-    new_model = get_model(model_config, new_dataset)
-    new_trainer = get_trainer(trainer_config, new_dataset, new_model)
-    initial_parameter(new_trainer, model)
-    new_trainer.train(verbose=True, writer=writer)
-    writer.close()
-    print('Part Retrain!')
-    new_trainer.retrain_eval(dataset.n_users, dataset.n_items)
-    rec_items = new_trainer.get_rec_items('train', None, k=100)[:dataset.n_users, :]
-    overlap_rate = cal_overlap_rate_array(rec_items, full_rec_items)
-    print('Overlap rate between full and part retrain: {:.3f}\n'.format(overlap_rate * 100))
-    new_rec_items = get_new_rec_items(rec_items, old_rec_items)
-    recall = cal_recall_set(new_rec_items, full_retrain_new_rec_items)
-    print('Recall between full and part retrain: {:.3f}\n'.format(recall * 100))
+    for n_epochs in [10, 20, 50]:
+        trainer_config['n_epochs'] = n_epochs
+        writer = SummaryWriter(os.path.join(log_path, 'part_retrain' + str(n_epochs)))
+        new_model = get_model(model_config, new_dataset)
+        new_trainer = get_trainer(trainer_config, new_dataset, new_model)
+        new_trainer.train(verbose=True, writer=writer)
+        writer.close()
+        print('Limited full Retrain!')
+        new_trainer.retrain_eval(dataset.n_users, dataset.n_items)
+        rec_items = new_trainer.get_rec_items('train', None, k=100)[:dataset.n_users, :]
+        new_rec_items = get_new_rec_items(rec_items, old_rec_items)
+        recall = cal_recall_set(new_rec_items, full_retrain_new_rec_items)
+        print('Recall of limited full retrain: {:.3f}, n_epochs {:d}\n'.format(recall * 100, n_epochs))
 
-    writer = SummaryWriter(os.path.join(log_path, 'pp_retrain_1'))
-    train_data = new_dataset.train_data
-    additional_train_data = [[item for item in train_data[u] if item > dataset.n_items] for u in range(dataset.n_users)]
-    additional_train_data += [train_data[u] for u in range(dataset.n_users, new_dataset.n_users)]
-    new_dataset.train_data = additional_train_data
-    new_model = get_model(model_config, new_dataset)
-    trainer_config['parameter_propagation'] = 2
-    new_trainer = get_trainer(trainer_config, new_dataset, new_model)
-    initial_parameter(new_trainer, model)
-    new_trainer.train(verbose=True, writer=writer)
-    writer.close()
+        writer = SummaryWriter(os.path.join(log_path, 'part_retrain' + str(n_epochs)))
+        new_model = get_model(model_config, new_dataset)
+        new_trainer = get_trainer(trainer_config, new_dataset, new_model)
+        initial_parameter(new_trainer, model)
+        new_trainer.train(verbose=True, writer=writer)
+        writer.close()
+        print('Part Retrain!')
+        new_trainer.retrain_eval(dataset.n_users, dataset.n_items)
+        rec_items = new_trainer.get_rec_items('train', None, k=100)[:dataset.n_users, :]
+        new_rec_items = get_new_rec_items(rec_items, old_rec_items)
+        recall = cal_recall_set(new_rec_items, full_retrain_new_rec_items)
+        print('Recall of part retrain: {:.3f}, n_epochs {:d}\n'.format(recall * 100, n_epochs))
 
-    writer = SummaryWriter(os.path.join(log_path, 'pp_retrain_2'))
-    new_dataset.train_data = train_data
-    new_trainer = get_trainer(trainer_config, new_dataset, new_model)
-    new_trainer.train(verbose=True, writer=writer)
-    writer.close()
-    print('Retrain with parameter propagation!')
-    new_trainer.retrain_eval(dataset.n_users, dataset.n_items)
-    rec_items = new_trainer.get_rec_items('train', None, k=100)[:dataset.n_users, :]
-    overlap_rate = cal_overlap_rate_array(rec_items, full_rec_items)
-    print('Overlap rate between full retrain and parameter propagation:{:.3f}\n'.format(overlap_rate * 100))
-    new_rec_items = get_new_rec_items(rec_items, old_rec_items)
-    recall = cal_recall_set(new_rec_items, full_retrain_new_rec_items)
-    print('Recall between full retrain and parameter propagation:{:.3f}\n'.format(recall * 100))
+        trainer_config['parameter_propagation'] = 2
+        trainer_config['lr'] = 2.e-4
+        trainer_config['n_epochs'] = trainer_config['n_epochs'] // 2
+        writer = SummaryWriter(os.path.join(log_path, 'pp_retrain_1' + str(n_epochs)))
+        train_data = new_dataset.train_data
+        additional_train_data = [[item for item in train_data[u] if item > dataset.n_items] for u in
+                                 range(dataset.n_users)]
+        additional_train_data += [train_data[u] for u in range(dataset.n_users, new_dataset.n_users)]
+        new_dataset.train_data = additional_train_data
+        new_model = get_model(model_config, new_dataset)
+        new_trainer = get_trainer(trainer_config, new_dataset, new_model)
+        initial_parameter(new_trainer, model)
+        new_trainer.train(verbose=True, writer=writer)
+        writer.close()
+
+        writer = SummaryWriter(os.path.join(log_path, 'pp_retrain_2' + str(n_epochs)))
+        new_dataset.train_data = train_data
+        new_trainer = get_trainer(trainer_config, new_dataset, new_model)
+        new_trainer.train(verbose=True, writer=writer)
+        writer.close()
+        print('Retrain with parameter propagation!')
+        new_trainer.retrain_eval(dataset.n_users, dataset.n_items)
+        rec_items = new_trainer.get_rec_items('train', None, k=100)[:dataset.n_users, :]
+        new_rec_items = get_new_rec_items(rec_items, old_rec_items)
+        recall = cal_recall_set(new_rec_items, full_retrain_new_rec_items)
+        print('Recall of parameter propagation:{:.3f}, n_epochs {:d}\n'.format(recall * 100, n_epochs))
 
 
 if __name__ == '__main__':
